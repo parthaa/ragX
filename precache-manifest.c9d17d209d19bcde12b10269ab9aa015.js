c2067316eb49780dc261273c6351224fb25e3d7d
Partha Aji <paji@redhat.com>
2019-05-10 01:06:42 -0400

    Updates

---
+++ precache-manifest.c9d17d209d19bcde12b10269ab9aa015.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "328460f5fc1dbdb422f151c762a47096",
+    "url": "/ragX/static/media/piano.328460f5.jpeg"
+  },
+  {
+    "revision": "8491bc8d22cee4091014",
+    "url": "/ragX/static/js/runtime~main.c385b65b.js"
+  },
+  {
+    "revision": "81495c1a922187b39638",
+    "url": "/ragX/static/js/main.b733fe56.chunk.js"
+  },
+  {
+    "revision": "0d23b7226418724426a0",
+    "url": "/ragX/static/js/2.6a509999.chunk.js"
+  },
+  {
+    "revision": "81495c1a922187b39638",
+    "url": "/ragX/static/css/main.fbe9cfb5.chunk.css"
+  },
+  {
+    "revision": "31ffd023c752ea394d8bfc0592833d79",
+    "url": "/ragX/index.html"
+  }
+];
